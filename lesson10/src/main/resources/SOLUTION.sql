27cf8ccad95d060ad2242191db68a0b98f8d21a2
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-06-05 18:50:31 +0300

    -1

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,2 +1,2 @@
 SELECT * FROM Subject WHERE id IN (SELECT subject_id FROM Mark GROUP BY subject_id HAVING AVG(mark) > (SELECT AVG(mark) FROM Mark));
-SELECT * FROM student WHERE id IN (SELECT student_id FROM (SELECT student_id, SUM(amount) AS sum_amount FROM payment GROUP BY student_id) AS payment_totals WHERE sum_amount < SELECT AVG(sum_amount) FROM  SELECT student_id, SUM(amount) AS sum_amount FROM payment GROUP BY student_id) AS payment_totals_avg);
+SELECT s.name FROM student s JOIN payment p ON p.student_id = s.id WHERE p.amount < (SELECT AVG(amount) FROM payment);

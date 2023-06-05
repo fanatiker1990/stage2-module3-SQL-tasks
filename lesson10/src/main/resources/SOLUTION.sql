3eeec2247947ea06c55aede80dc1e00d674c6d73
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-06-05 20:36:43 +0300

    -1

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,2 +1,2 @@
 SELECT * FROM Subject WHERE id IN (SELECT subject_id FROM Mark GROUP BY subject_id HAVING AVG(mark) > (SELECT AVG(mark) FROM Mark));
-SELECT s.* FROM student s JOIN payment p ON s.id = p.student_id WHERE p.amount < (SELECT AVG(amount) FROM payment);
+SELECT DISTINCT s.* FROM student s JOIN payment p ON s.id = p.student_id WHERE p.amount < (SELECT AVG(amount) FROM payment);
--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,6 +1,4 @@
-DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE grade > 4);
-DELETE FROM payment WHERE student_id IN (SELECT id FROM student WHERE grade > 4);
-DELETE FROM student WHERE grade > 4;
+DELETE FROM student WHERE id IN (SELECT student.id FROM student LEFT JOIN mark ON student.id = mark.student_id LEFT JOIN subject ON subject.id = mark.subject_id WHERE subject.grade > 4);
 DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4));
 DELETE FROM payment WHERE student_id IN (SELECT id FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4));
 DELETE FROM student WHERE id IN (SELECT id FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4));

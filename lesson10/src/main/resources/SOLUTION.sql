d99e9d45ddcbb49ec55f165615512c92031dc37d
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-06-05 19:32:23 +0300

    -1

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,2 +1,2 @@
 SELECT * FROM Subject WHERE id IN (SELECT subject_id FROM Mark GROUP BY subject_id HAVING AVG(mark) > (SELECT AVG(mark) FROM Mark));
-SELECT s.name FROM student s JOIN payment p ON p.student_id = s.id WHERE p.amount < (SELECT AVG(amount) FROM payment);
+SELECT s.* FROM student s JOIN payment p ON s.id = p.student_id WHERE p.amount < (SELECT AVG(amount) FROM payment);
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,6 +2,6 @@ SELECT * FROM Payment WHERE amount >= 500;
 SELECT * FROM Student WHERE DATEADD(YEAR, -20, CURRENT_DATE) > birthday;
 SELECT * FROM Student WHERE groupnumber = 10 AND DATEADD(YEAR, -20, CURRENT_DATE) < birthday;
 SELECT * FROM Student WHERE name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
-SELECT * FROM Payment WHERE DATEADD(MONTH, -8, CURRENT_DATE) < payment_date;
+SELECT * FROM payment WHERE payment_date >= DATEADD(month, -8, CURRENT_DATE);
 SELECT * FROM Student WHERE name LIKE 'A%';
 SELECT * FROM Student WHERE (name LIKE 'Roxi%' AND groupnumber = 4) OR (name LIKE 'Tallie%' AND groupnumber = 9);

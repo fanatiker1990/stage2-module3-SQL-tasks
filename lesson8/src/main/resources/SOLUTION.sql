b6442c04fa3279c9bc7608094e8cb9491533a6a2
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-06-05 14:31:58 +0300

    -1

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,23 +1,4 @@
-SELECT MAX(birthday) AS max_date_of_birth
-FROM student;
-SELECT *
-FROM payment
-WHERE DATE(payment_date) = (
-    SELECT MIN(DATE(payment_date))
-    FROM payment
-);
-SELECT AVG(mark) AS average_mark
-FROM mark
-WHERE subject_id = (
-    SELECT id
-    FROM subject
-    WHERE name LIKE 'Math%'
-)
-HAVING COUNT(*) > 0;
-SELECT MIN(amount) AS min_payment_amount
-FROM payment
-WHERE type_id = (
-    SELECT id
-    FROM paymenttype
-    WHERE name = 'WEEKLY'
-);
+SELECT MAX(birthday) AS max_date_of_birth FROM student;
+SELECT * FROM payment WHERE DATE(payment_date) = ( SELECT MIN(DATE(payment_date)) FROM payment);
+SELECT AVG(mark) AS average_mark FROM mark WHERE subject_id = (   SELECT id FROM subject WHERE name LIKE 'Math%') HAVING COUNT(*) > 0;
+SELECT MIN(amount) AS min_payment_amount FROM payment WHERE type_id = ( SELECT id FROM paymenttype  WHERE name = 'WEEKLY');

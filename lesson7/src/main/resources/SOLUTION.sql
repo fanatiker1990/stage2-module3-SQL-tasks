d7770df00ffe1153cffedf11cef86560e419e99a
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-06-05 11:43:07 +0300

    2

--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
-select*from mark where mark>6 order by  mark desc;
-select* from payment where amount< 300 order by amount asc ;
-select* from paymenttype order by name asc;
-select *from student order by name desc;
-SELECT * FROM Student WHERE id IN (SELECT student_id FROM Payment WHERE amount > 1000)ORDER BY birthday ASC;
+SELECT * FROM mark WHERE mark > 6 ORDER BY mark DESC;
+SELECT * FROM payment WHERE amount < 300 ORDER BY amount ASC;
+SELECT * FROM paymenttype ORDER BY name ASC;
+SELECT * FROM student ORDER BY name DESC;
+SELECT * FROM student WHERE id IN (SELECT student_id FROM Payment WHERE amount > 1000) ORDER BY birthday ASC;
